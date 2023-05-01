7dab4571050588a2b742f0d6fb86689f0ea04a9b
getchagrooveon <126591540+getchagrooveon@users.noreply.github.com>
2023-05-01 14:16:57 +0000

    Deploying to gh-pages from @ getchagrooveon/goit-js-hw-08@4137bda2c3412e406073d28298b5c23d7b0d99dc 🚀

---
+++ 03-feedback.2e4ac6b4.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".feedback-form");document.querySelector('[type="email"]'),document.querySelector('[name="message"]'),document.querySelector('[type="submit"]');e.addEventListener("input",(e=>{console.log(e.currentTarget.value)}));
+//# sourceMappingURL=03-feedback.2e4ac6b4.js.map
---
+++ 03-feedback.80f67104.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".feedback-form");document.querySelector('[type="email"]'),document.querySelector('[name="message"]'),document.querySelector('[type="submit"]');e.addEventListener("input",(function(e){console.log(e.currentTarget.value)}))}();
+//# sourceMappingURL=03-feedback.80f67104.js.map
