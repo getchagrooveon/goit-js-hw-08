7155f71276b988d40e28f3b0973c900a78d66a13
getchagrooveon <126591540+getchagrooveon@users.noreply.github.com>
2023-05-02 14:35:02 +0000

    Deploying to gh-pages from @ getchagrooveon/goit-js-hw-08@9d59e220f27c2fc3d6d9ce5714da8f0ce504075d 🚀

--- 03-feedback.2e4ac6b4.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".feedback-form");document.querySelector('[type="email"]'),document.querySelector('[name="message"]'),document.querySelector('[type="submit"]');e.addEventListener("input",(e=>{console.log(e.currentTarget.value)}));
-//# sourceMappingURL=03-feedback.2e4ac6b4.js.map
