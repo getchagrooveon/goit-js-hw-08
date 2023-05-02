9d59e220f27c2fc3d6d9ce5714da8f0ce504075d
getchagrooveon <eugene.osipov.1989@gmail.com>
2023-05-02 17:34:35 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,19 +1,31 @@
+import throttle from 'lodash.throttle'
 const feedbackForm = document.querySelector('.feedback-form')
 const email = document.querySelector('[type="email"]')
 const message = document.querySelector('[name="message"]')
 const submit = document.querySelector('[type="submit"]')
 
+const data = {}
+
+const storage = localStorage.getItem('feedback-form-state')
+
+if (storage) {
+    const {message, email} = JSON.parse(storage)
+    message.value = message
+    email.value = email
+}
+
+function onInput() {
+    data.email = email.value
+    data.message = message.value
+    localStorage.setItem('feedback-form-state', `${JSON.stringify(data)}`)
+    }
 
 
-feedbackForm.addEventListener("input", (event) => {
-    const currentData = {}
-    console.log(event.currentTarget.value);
+feedbackForm.addEventListener("input", throttle(onInput, 500))
+
+feedbackForm.addEventListener("submit", (event) => {
+    event.preventDefault()
+    localStorage.removeItem('feedback-form-state')
+    console.log(data)
+    event.target.reset()
 })
-
-
-
-// HTML містить розмітку форми. Напиши скрипт, який буде зберігати значення полів у локальне сховище, коли користувач щось друкує.
-// Відстежуй на формі подію input, і щоразу записуй у локальне сховище об'єкт з полями email і message, у яких зберігай поточні значення полів форми. Нехай ключем для сховища буде рядок "feedback-form-state".
-// Під час завантаження сторінки перевіряй стан сховища, і якщо там є збережені дані, заповнюй ними поля форми. В іншому випадку поля повинні бути порожніми.
-// Під час сабміту форми очищуй сховище і поля форми, а також виводь у консоль об'єкт з полями email, message та їхніми поточними значеннями.
-// Зроби так, щоб сховище оновлювалось не частіше, ніж раз на 500 мілісекунд. Для цього додай до проекту і використовуй бібліотеку lodash.throttle.
